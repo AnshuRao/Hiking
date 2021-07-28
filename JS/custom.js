ca12c79050e4b2d3216cfad8672b93f2f0c19433
AnshuRao <rao.anshu38@gmail.com>
2021-07-28 11:14:54 +0530

    new tilt

--- JS/custom.js
+++ JS/custom.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
 });
 
 VanillaTilt.init(document.querySelectorAll(".box"), {
-    max: 25,
-    speed: 400
+    max: 30,
+    speed: 400,
+    gyroscope:              true,    // Boolean to enable/disable device orientation detection,
+    gyroscopeMinAngleX:     -45,     // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+    gyroscopeMaxAngleX:     45,      // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+    gyroscopeMinAngleY:     -45,     // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+    gyroscopeMaxAngleY:     45,   
 });
 
